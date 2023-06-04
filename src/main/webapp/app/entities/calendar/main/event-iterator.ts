a5668dc38740477bf2e967bf17cec9a5cd3cd168
Tulbu Gabriela <gabriela.tulbu@gmail.com>
2023-06-04 10:48:05 +0300

    last commit

--- src/main/webapp/app/entities/calendar/main/event-iterator.ts
+++ src/main/webapp/app/entities/calendar/main/event-iterator.ts
@@ -1,29 +1,34 @@
-import {IEvent} from "../../event/event.model";
-
-export class EventIterator {
-  private readonly events: IEvent[] = []
-  private position = 0
+import { IEvent } from '../../event/event.model';
+export interface IEventIterator {
+  hasNext(): boolean;
+  next(): IEvent;
+  hasPrev(): boolean;
+  prev(): IEvent;
+}
+export class EventIterator implements IEventIterator {
+  private readonly events: IEvent[] = [];
+  private position = 0;
 
   constructor(events: IEvent[]) {
-    this.events = events
+    this.events = events;
   }
 
   hasNext(): boolean {
-    return this.position < this.events.length
+    return this.position < this.events.length;
   }
 
   next(): IEvent {
-    const event = this.events[this.position]
-    this.position ++
-    return event
+    const event = this.events[this.position];
+    this.position++;
+    return event;
   }
 
   hasPrev(): boolean {
-    return this.position <= this.events.length && this.position != 1
+    return this.position <= this.events.length && this.position != 1;
   }
 
   prev(): IEvent {
-    this.position --
-    return this.events[this.position]
+    this.position--;
+    return this.events[this.position];
   }
 }
